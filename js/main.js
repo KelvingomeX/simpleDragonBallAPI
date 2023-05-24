17a9b8c56c79656da7e1df0575c1ab0d3fc9c373
kelvingomex <kelvingomex202@gmail.comcd>
2023-05-23 20:04:46 -0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+async function run(){
+    try {
+      const character = document.querySelector("input").value;
+      const res = await fetch(`/api/${character}`);
+      const data = await res.json();
+      
+      console.log(data);
+      // document.querySelector("#personName").textContent = data.
+      // document.querySelector("#personStatus").textContent = data.status
+      // document.querySelector("#personOccupation").textContent = data.currentOccupation
+    } catch(error) {
+      console.error(error)
+    }
+  }
+  document.querySelector("button").addEventListener("click", run);
